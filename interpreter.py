6f79d109bb59587b591e8633a281591a61c62b89
obsessed <obsessed776@gmail.com>
2026-09-10 14:04:30 +0000

    add printing to console

--- interpreter.py
+++ interpreter.py
@@ -9,9 +9,28 @@ def read_file(filename):
 
  
 def interpreter(raw_code):
-    pass
+    memory = [0] * 30_000
+    memory_pointer = 0
+    pointer = 0
     
- 
+    while pointer < len(raw_code):
+        instruction = raw_code[pointer]
+
+        match instruction:
+            case ">":
+                memory_pointer += 1
+            case "<":
+                memory_pointer -= 1
+            case "+":
+                memory[memory_pointer] += 1
+            case "-":
+                memory[memory_pointer] -= 1
+            case ".":
+                print(chr(memory[memory_pointer]), end="")
+            
+        pointer += 1
+
+    
 if __name__ == "__main__":
     raw_code = read_file("helloworld.bf")
-    
+    interpreter(raw_code)
