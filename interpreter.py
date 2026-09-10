bf0d82ce0b09276d9c7211f73e4b587071dfcabb
obsessed <obsessed776@gmail.com>
2026-09-10 14:15:22 +0000

    add loop

--- interpreter.py
+++ interpreter.py
@@ -29,10 +29,30 @@ def interpreter(raw_code):
                 print(chr(memory[memory_pointer]), end="")
             case ",":
                 memory[memory_pointer] = int(input("Input: "))
-            
+            case "[":
+                if memory[memory_pointer] == 0:
+                    nestend_index = 1
+                    while nestend_index > 0:
+                        pointer += 1
+                        if raw_code[pointer] == "[":
+                            nestend_index += 1
+                        elif raw_code[pointer] == "]":
+                            nestend_index -= 1
+            case "]":
+                if memory[memory_pointer] != 0:
+                    nestend_index = 1
+                    while nestend_index > 0:
+                        pointer -= 1
+                        if raw_code[pointer] == "]":
+                            nestend_index += 1
+                        elif raw_code[pointer] == "[":
+                            nestend_index -= 1
+            case _:
+                raise SyntaxError("Wrond command")
+                            
         pointer += 1
 
     
 if __name__ == "__main__":
-    raw_code = read_file("getting_input.bf")
+    raw_code = read_file("sum_two_numbers.bf")
     interpreter(raw_code)
